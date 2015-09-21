c6668842d6cca4ea8ec43fac653d846be85abb0a
Bohdan Kostko <bohdan.kostko@nopattern.net>
2015-09-21 16:47:48 +0200

    feat(gulp): Release task

--- gulpfile.js
+++ gulpfile.js
@@ -10,13 +10,19 @@ plugins.minimist = require('minimist')(process.argv.slice(2));
 
 var PATH = {
   bower_components: 'bower_components',
-  build: '.'
+  build: '.',
+  dist: './',
+};
+
+var BRANCH = {
+  master: 'master',
+  develop: 'develop'
 };
 
 var MAIN = 'angular-preload-image.js';
 
 gulp.task('minify', ['lint'], function() {
-  return gulp.src(path.join('.', PATH.build, MAIN))
+  return gulp.src(path.join(PATH.build, MAIN))
     .pipe(plugins.sourcemaps.init({loadMaps: true}))
     .pipe(plugins.uglify({
       preserveComments: 'all'
@@ -49,4 +55,50 @@ gulp.task('jscs', function() {
 
 gulp.task('develop', ['minify'], function() {
   gulp.watch(MAIN, ['lint']);
+});
+
+gulp.task('release::dist::commit', ['minify'], function() {
+  plugins.git.checkout(BRANCH.develop);
+
+  return gulp.src(PATH.dist)
+    .pipe(plugins.git.add())
+    .pipe(plugins.git.commit('feat(release): New release.'));
+});
+
+gulp.task('release::dist::merge', ['release::dist::commit'], function() {
+  return plugins.git.checkout(BRANCH.master, function() {
+    plugins.git.merge(BRANCH.develop);
+  });
+});
+
+gulp.task('release::dist::push', ['release::dist::merge'], function(done) {
+  return plugins.git.push('origin', BRANCH.develop, {cwd: PATH.dist}, function(err) {
+    if (err) {
+      plugins.git.checkout(BRANCH.master);
+
+      plugins.git.reset('HEAD~1', { args: '--hard' });
+
+      plugins.git.checkout(BRANCH.develop);
+
+      plugins.git.reset('HEAD~1', { args: '--hard' });
+
+      throw err;
+    }
+
+    plugins.git.push('origin', BRANCH.master, {cwd: PATH.dist}, done);
+  });
+});
+
+gulp.task('release::dist::tag', ['release::dist::push'], function(done) {
+  return plugins.git.tag('v' + pkg.version, 'v' + pkg.version, {cwd: PATH.dist}, function(err) {
+    if (err) {
+      throw err;
+    }
+
+    plugins.git.push('origin', 'refs/tags/v' + pkg.version, {cwd: PATH.dist}, done);
+  });
+});
+
+gulp.task('release', ['release::dist::tag'], function() {
+
 });
